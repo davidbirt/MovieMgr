c4e23764d4f1775ddfa25031169a9ad6f58fd97a
dbz <dbirt3168@outlook.com>
2017-09-15 16:57:35 -0400

    enhance: working with save button on edit page.. bootstrap button only shows on top of form

--- MovieManager.API/www/app.js
+++ MovieManager.API/www/app.js
@@ -1,4 +1,4 @@
 ﻿angular.module('movies', ['ngRoute', 'angularUtils.directives.dirPagination', function ($routeProvider) {
 	$routeProvider.when('/', { templateUrl: '/www/views/movies-list.html' });
-	$routeProvider.when("/edit/:movieId", { templateUrl: "www/views/movies-edit.html" })
+	$routeProvider.when("/edit/:movieId", { templateUrl: "www/views/movies-edit.html?v=1.1" })
 }]);
